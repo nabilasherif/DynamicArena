49059c84c9aaeae293e4d2f0e0764e018cf37bf7
salmarashad <salmamarashad@gmail.com>
2024-12-10 14:33:50 +0200

    memoised

--- src/main/java/csen703/main/assignment2/DynamicArena.java
+++ src/main/java/csen703/main/assignment2/DynamicArena.java
@@ -4,30 +4,44 @@ public class DynamicArena {
 
 
     public static int ClimbDynamicArenaDP(int [] floors){
-        // 7asa 2 arrays one for if i can enter one for if i can't ?
-        // momken 1 2d array instead bardo still need to think shwaya
-        return helper(floors, 0, true);
+        int [][] memo = new int [floors.length][2];
+        for(int i =0; i < floors.length; i++){
+            for(int j = 0; j < 2; j++){
+                memo[i][j] = Integer.MIN_VALUE;
+            }
+        }
+        return helper(floors, 0, 1, memo);
     }
 
-    public static int helper(int[] floors, int index, boolean firstMove) {
+    public static int helper(int[] floors, int index, int firstMove, int[][] memo) {
         if (index >= floors.length) {
             return 0;
         }
 
-        if (firstMove) {
-            return Math.max(helper(floors, index + 1, false) - floors[index],
-                    helper(floors, index + 1, true));
-        } else {
-            return Math.max(helper(floors, index + 2, true) + floors[index],
-                    helper(floors, index + 1, false)
-            );
+        if (memo[index][firstMove] != Integer.MIN_VALUE) {
+            return memo[index][firstMove];
         }
+
+        int result;
+
+        if (firstMove == 1) {
+            result = Math.max(helper(floors, index + 1, 0, memo) - floors[index],
+                    helper(floors, index + 1, 1, memo));
+        } else {
+            result = Math.max(helper(floors, index + 2, 1, memo) + floors[index],
+                    helper(floors, index + 1, 0, memo));
+        }
+
+        memo[index][firstMove] = result;
+
+        return result;
     }
 
-    public static void main(String[] args) {
-        int[] floors = {48,12,60,93,97,42,25,64,17,56,85,93,9,48,52,42,58,85,81,84,69,36,1,54,23,15,72,15,11,94};
-        System.out.println(ClimbDynamicArenaDP(floors));
-    }
+
+//    public static void main(String[] args) {
+//        int[] floors = {48,12,60,93,97,42,25,64,17,56,85,93,9,48,52,42,58,85,81,84,69,36,1,54,23,15,72,15,11,94};
+//        System.out.println(ClimbDynamicArenaDP(floors));
+//    }
 
 
 }
